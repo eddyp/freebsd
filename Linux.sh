a030b161910f2c09b3c5c75ee21bdcdf9da7dec8
Eddy Petrișor <eddy.petrisor@gmail.com>
2017-09-26 13:47:53 +0300

    tools/linux-wrappers: wrappers to make the Linux tools look like FreeBSD's
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- Linux.sh
+++ Linux.sh
@@ -2,15 +2,17 @@ export FBSDROOT=$(pwd)
 export HOSTDIR=${FBSDROOT}/linuxhd
 
 export HOSTBIN=${HOSTDIR}/bin
+export LINUXWRAPPERSDIR=$FBSDROOT/tools/linux-wrappers
+
+export PATH=$HOSTBIN:$LINUXWRAPPERSDIR:$PATH
+
 export MAKE=${HOSTBIN}/bmake
 if [ ! -x $MAKE ]; then
 	(cd contrib/bmake && ./boot-strap --install-prefix=$HOSTDIR --prefix=$HOSTDIR --with-default-sys-path=$FBSDROOT/share/mk -DWITH_PROG_VERSION)
 fi
-export PATH=$HOSTBIN:$PATH
-UNAME_P=$(uname -p)
-[ $UNAME_P = x86_64 ] && MACHINE_CPUARCH=amd64 || MACHINE_CPUARCH=$UNAME_P
-export MACHINE=$(uname -m)
-export MACHINE_CPUARCH
+export HOST_MACHINE=$(uname -m)
+#MACHINE_CPUARCH=$(uname -p)
+export HOST_MACHINE_ARCH=$MACHINE_CPUARCH
 export MAKEFLAGS="-m share/mk -dmlxVtMA"
 export COMPILER_TYPE=gcc
 export SHELL=/usr/bin/tcsh
