a5c2aeff87a463cba9ce95f9d7f91dbf2f94a466
Eddy Petrișor <eddy.petrisor@gmail.com>
2017-09-26 10:14:28 +0300

    Linux.sh: generate bmake more cleanly and prepare for buildworld
    bmake build is still not 100% clean since contrib/linux4-x86_64 is still
    generated, but this provides consistent results if ran by:
    
    (. Linux.sh ; ${MAKE} TARGET=$MACHINE_CPUARCH TARGET_ARCH=$MACHINE_CPUARCH BUILD_ARCH=$MACHINE_CPUARCH buildworld)
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- Linux.sh
+++ Linux.sh
@@ -1,7 +1,16 @@
-export MAKE=$(pwd)/linux/linux4-x86_64/bin/bmake
+export FBSDROOT=$(pwd)
+export HOSTDIR=${FBSDROOT}/linuxhd
+
+export HOSTBIN=${HOSTDIR}/bin
+export MAKE=${HOSTBIN}/bmake
+if [ ! -x $MAKE ]; then
+	(cd contrib/bmake && ./boot-strap --install-prefix=$HOSTDIR --prefix=$HOSTDIR --with-default-sys-path=$FBSDROOT/share/mk -DWITH_PROG_VERSION)
+fi
+export PATH=$HOSTBIN:$PATH
+UNAME_P=$(uname -p)
+[ $UNAME_P = x86_64 ] && MACHINE_CPUARCH=amd64 || MACHINE_CPUARCH=$UNAME_P
+export MACHINE=$(uname -m)
+export MACHINE_CPUARCH
 export MAKEFLAGS="-m share/mk -dmlxVtMA"
 export COMPILER_TYPE=gcc
 export SHELL=/usr/bin/tcsh
-UNAME_P=$(uname -p)
-[ $UNAME_P = x86_64 ] && MACHINE_CPUARCH=amd64 || MACHINE_CPUARCH=$UNAME_P
-export MACHINE_CPUARCH
